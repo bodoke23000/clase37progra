3844da58d2cd1f2434eae57cf2d5aef08ffdfe87
Maximiliano <maxi.ariaso@duocuc.cl>
2026-06-24 10:55:44 -0400

    validar password

--- modulo_usuario.py
+++ modulo_usuario.py
@@ -8,20 +8,21 @@ def validar_sexo(sexo):
         return False
 
 def validar_password(password):
-    if " " in password.strip().lower():
-        print("Error, la contraseña no debe llevar espacios en blanco")
+    if len(password.strip())<8:
+        print("Error, minimo 8 caracteres")
         return False
-    elif len(password.strip().lower())<8:
-        print("Error, la contraseña debe tener almenos 8 caracteres")
+    tiene_numero=False
+    for numero in password:
+        if numero.isnumeric():
+            tiene_numero=True
+    tiene_letra=False
+    for letra in password:
+        if letra.isalpha():
+            tiene_letra=True
+    if " " in password:
+        print("Error, no puede llevar espacios en blanco")
         return False
-    elif [1,2,3,4,5,6,7,8,9,0] not in password.strip().lower():
-        print("Error, la contraseña debe tener al menos 1 numero")
-        return False
-    elif ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","ñ","o","p","q","r","s","t","u","v","w","x","y","z"] not in password.strip().lower():
-        print("Error, la contraseña debe tener al menos una letra")
-        return False
-    else:
-        return True
+    return tiene_letra and tiene_numero
 
 def imprimir_usuario(usuario):
     print(f"""
@@ -38,17 +39,17 @@ def validar_texto_vacio(texto):
         return False
 
 def agregrar_usuario():
-    nombre_usuario=str(input("Ingrese el nombre de usuario:\n")).strip().lower()
+    nombre_usuario=str(input("Ingrese el nombre de usuario:\n")).strip()
     while not validar_texto_vacio(nombre_usuario):
-        nombre_usuario=str(input("Ingrese el nombre de usuario:\n")).strip().lower()
+        nombre_usuario=str(input("Ingrese el nombre de usuario:\n")).strip()
     
     sexo=str(input("Ingrese su sexo:\n")).strip()
     while not validar_sexo(sexo):
         sexo=str(input("Ingrese su sexo:\n")).strip()
     
-    password=str(input("Ingrese su contraseña:\n")).strip().lower()
+    password=str(input("Ingrese su contraseña:\n")).strip()
     while not validar_password(password):
-        password=str(input("Ingrese su contraseña:\n")).strip().lower()
+        password=str(input("Ingrese su contraseña:\n")).strip()
     
     usuario={
         "nombre":nombre_usuario,
